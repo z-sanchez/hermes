6f1f14534808d2010171eacf8e918d697939719a
Ziek <eas260@txstate.edu>
2022-02-17 07:41:59 -0600

    Tweaked css for active contact design by getting rid of border radius to the left side of indicator bar. Fixed clock display so it doesn't show 0 in front of hour number and also added AM or PM display

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -1,42 +1,45 @@
 import React from "react";
 
 class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: "clock",
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: "clock",
+        };
+    }
 
-  componentDidMount() {
-    this.ticker = setInterval(() => {
-      this.clock();
-    }, 1000);
-  }
+    componentDidMount() {
+        this.ticker = setInterval(() => {
+            this.clock();
+        }, 1000);
+    }
 
-  componentWillUnmount() {
-    clearInterval(this.ticker);
-  }
+    componentWillUnmount() {
+        clearInterval(this.ticker);
+    }
 
-  clock() {
-    const today = new Date();
-    let hour = today.getHours(),
-      minute = today.getMinutes();
+    clock() {
+        const today = new Date();
+        let hour = today.getHours(),
+            minute = today.getMinutes(),
+            meridiem = 'AM';
 
-    if (hour === 12) hour = 12;
-    if (hour > 12) hour = hour - 12;
+        if (hour === 12) {
+            hour = 12;
+            meridiem = 'PM';
+        }
+        if (hour > 12) hour = hour - 12;
 
-    hour = hour < 10 ? "0" + hour : hour;
-    minute = minute < 10 ? "0" + minute : minute;
+        minute = minute < 10 ? "0" + minute : minute;
 
-    this.setState({
-      time: hour + ":" + minute,
-    });
-  }
+        this.setState({
+            time: hour + ":" + minute + meridiem,
+        });
+    }
 
-  render() {
-    return <p id="clock">{this.state.time}</p>;
-  }
+    render() {
+        return <p id="clock">{this.state.time}</p>;
+    }
 }
 
 export default Clock;
