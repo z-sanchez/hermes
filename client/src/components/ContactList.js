8e596f8d1fb1cb41efd6fbd2b2d5b7d852f1f47c
Ziek Sanchez <zieksanchez3@gmail.com>
2022-03-03 17:11:36 -0600

    add button now stays lit when adding contacts

--- client/src/components/ContactList.js
+++ client/src/components/ContactList.js
@@ -5,190 +5,194 @@ import uniqid from "uniqid";
 import dropDownArrow from "../images/dropdownArrow.svg";
 
 function isMobile() {
-    return window.innerWidth < 1400;
+  return window.innerWidth < 1400;
 }
 
 class ContactList extends React.Component {
-    static contextType = DatabaseContext;
+  static contextType = DatabaseContext;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            contacts: [],
-            adding: false,
-            users: null,
-            searchValue: null,
-            toggleList: false,
-            isMobile: false,
-        };
-    }
-
-    componentDidMount() {
-        this.getContacts();
-        this.getUsers();
-
-        if (isMobile()) {
-            this.setState({
-                isMobile: true,
-            })
-        }
-
-        window.addEventListener('resize', () => {
-            if (isMobile()) this.setState({
-                isMobile: true,
-            })
-            else {
-                this.setState({
-                    isMobile: false,
-                })
-            }
-        });
-    }
-
-    componentWillUnmount() {
-        this.getContactsQuery();
-        this.getUsersQuery();
-    }
-
-    getContacts = () => {
-        //grabs all user contacts from database
-        let contacts = this.context.database.collection(this.context.uid);
-        this.getContactsQuery = contacts.onSnapshot((querySnapshot) => {
-            const data = querySnapshot.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
-
-            if (data) {
-                this.setState({
-                    contacts: data,
-                });
-            }
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      adding: false,
+      users: null,
+      searchValue: null,
+      toggleList: false,
+      isMobile: false,
     };
+  }
 
-    getUsers = () => {
-        //grabs all user contacts from database
-        let contacts = this.context.database.collection("users");
-        this.getUsersQuery = contacts.onSnapshot((querySnapshot) => {
-            const data = querySnapshot.docs.map((doc) => ({
-                ...doc.data(),
-                id: doc.id,
-            }));
+  componentDidMount() {
+    this.getContacts();
+    this.getUsers();
 
-            if (data) {
-                this.setState({
-                    users: data,
-                });
-            }
-        });
-    };
+    if (isMobile()) {
+      this.setState({
+        isMobile: true,
+      });
+    }
 
-    setStateAdding = () => {
+    window.addEventListener("resize", () => {
+      if (isMobile())
         this.setState({
-            adding: !this.state.adding,
+          isMobile: true,
         });
-    };
-
-    renderContacts() {
-        let contactsToRender = [];
-        this.state.adding
-            ? (contactsToRender = this.state.users)
-            : (contactsToRender = this.state.contacts);
-
-        if (this.state.adding === false && this.state.contacts.length === 0) {
-            return (
-                <div id="contactsList">
-                    <p id="noContacts">No Contacts</p>
-                </div>
-            );
-        }
-
-        return contactsToRender.map((contact) => {
-            if (contact.uid === this.context.uid) return null;
-
-            if (this.state.searchValue !== null) {
-                if (!contact.name.includes(this.state.searchValue)) return null;
-            }
-
-            return (
-                <Contact
-                    key={uniqid()}
-                    adding={this.state.adding}
-                    contactData={contact}
-                    currentContact={this.props.currentContact}
-                    toggleList={this.toggleList}
-                />
-            );
-        });
-    }
-
-    toggleList = () => {
+      else {
         this.setState({
-            toggleList: !this.state.toggleList,
+          isMobile: false,
         });
-    };
+      }
+    });
+  }
 
-    renderMobileList = () => {
-        let contactHeader = this.props.currentContact.currentContactName;
-        if (contactHeader === null) contactHeader = "No Contact";
+  componentWillUnmount() {
+    this.getContactsQuery();
+    this.getUsersQuery();
+  }
 
-        let contacts = null;
+  getContacts = () => {
+    //grabs all user contacts from database
+    let contacts = this.context.database.collection(this.context.uid);
+    this.getContactsQuery = contacts.onSnapshot((querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
 
-        if (this.state.toggleList) contacts = this.renderContacts();
-        //render this. Edit classes and ids to display none in media queries.
-        // Make proper css adjustments to keep spacing consistent. For contacts change media query to display properly
-        return (
-            <div id="receiver">
-                <div id="receiver__header">
-                    <p>{contactHeader}</p>
-                    <img
-                        src={dropDownArrow}
-                        id="dropDown"
-                        alt="drop down"
-                        onClick={this.toggleList}
-                    />
-                </div>
-                <div id="contactsFlex">
-                    {contacts}
-                </div>
-            </div>
-        );
-    };
+      if (data) {
+        this.setState({
+          contacts: data,
+        });
+      }
+    });
+  };
 
-    render() {
-        let contactList = <div id="contactsList">{this.renderContacts()}</div>;
+  getUsers = () => {
+    //grabs all user contacts from database
+    let contacts = this.context.database.collection("users");
+    this.getUsersQuery = contacts.onSnapshot((querySnapshot) => {
+      const data = querySnapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
 
-        if (this.props.mobile) {
-            return this.renderMobileList();
-        }
+      if (data) {
+        this.setState({
+          users: data,
+        });
+      }
+    });
+  };
 
-        return (
-            <div id="contacts">
-                <div id="buttonBar">
-                    <input
-                        type="search"
-                        id="searchBar"
-                        placeholder="Search ..."
-                        onChange={(e) => {
-                            this.setState({
-                                searchValue: e.target.value,
-                            });
-                        }}
-                    />
+  setStateAdding = (button) => {
+    if (this.state.adding) button.style.backgroundColor = "#583E79";
+    else button.style.backgroundColor = "#9430EB";
 
-                    <button
-                        id="addButton"
-                        className="buttons"
-                        onClick={this.setStateAdding}
-                    >
-                        Add
-                    </button>
-                </div>
-                {contactList}
-            </div>
-        );
+    this.setState({
+      adding: !this.state.adding,
+    });
+  };
+
+  renderContacts() {
+    let contactsToRender = [];
+    this.state.adding
+      ? (contactsToRender = this.state.users)
+      : (contactsToRender = this.state.contacts);
+
+    if (this.state.adding === false && this.state.contacts.length === 0) {
+      return (
+        <div id="contactsList">
+          <p id="noContacts">No Contacts</p>
+        </div>
+      );
     }
+
+    return contactsToRender.map((contact) => {
+      if (contact.uid === this.context.uid) return null;
+
+      if (this.state.searchValue !== null) {
+        if (!contact.name.includes(this.state.searchValue)) return null;
+      }
+
+      return (
+        <Contact
+          key={uniqid()}
+          adding={this.state.adding}
+          contactData={contact}
+          currentContact={this.props.currentContact}
+          toggleList={this.toggleList}
+        />
+      );
+    });
+  }
+
+  toggleList = () => {
+    this.setState({
+      toggleList: !this.state.toggleList,
+    });
+  };
+
+  renderMobileList = () => {
+    let contactHeader = this.props.currentContact.currentContactName;
+    if (contactHeader === null) contactHeader = "No Contact";
+
+    let contacts = null;
+
+    if (this.state.toggleList) contacts = this.renderContacts();
+    //render this. Edit classes and ids to display none in media queries.
+    // Make proper css adjustments to keep spacing consistent. For contacts change media query to display properly
+    return (
+      <div id="receiver">
+        <div id="receiver__header">
+          <p>{contactHeader}</p>
+          <img
+            src={dropDownArrow}
+            id="dropDown"
+            alt="drop down"
+            onClick={this.toggleList}
+          />
+        </div>
+        <div id="contactsFlex">{contacts}</div>
+      </div>
+    );
+  };
+
+  render() {
+    let contactList = <div id="contactsList">{this.renderContacts()}</div>;
+
+    if (this.props.mobile) {
+      return this.renderMobileList();
+    }
+
+    return (
+      <div id="contacts">
+        <div id="buttonBar">
+          <input
+            type="search"
+            id="searchBar"
+            placeholder="Search ..."
+            onChange={(e) => {
+              this.setState({
+                searchValue: e.target.value,
+              });
+            }}
+          />
+
+          <button
+            id="addButton"
+            className="buttons"
+            onClick={(button) => {
+              this.setStateAdding(button.target);
+            }}
+          >
+            Add
+          </button>
+        </div>
+        {contactList}
+      </div>
+    );
+  }
 }
 
 export default ContactList;
